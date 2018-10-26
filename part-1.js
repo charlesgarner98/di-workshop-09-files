7096fc98517d29cb00109706dae77389de1edd1e
Charles Garner <charles.garner@ada.ac.uk>
2018-10-26 14:49:09 +0100

    Finished Part 1

--- part-1.js
+++ part-1.js
@@ -1,1 +1,27 @@
-console.log('your code here')
+var fs = require('fs') //Require fs for file handling
+
+var fileContents = fs.readFileSync('names.txt', 'utf-8') //Read text from file
+console.log('-- File Content --')
+console.log(fileContents)
+
+var fileArray = fileContents.split('\r\n')
+console.log('-- File Array --')
+console.log(fileArray)
+
+var reversedArray = fileArray.reverse()
+console.log('-- Reversed Array --')
+console.log(reversedArray)
+
+var reversedContents = reversedArray.join('\r\n')
+console.log('-- Reversed Content --')
+console.log(reversedContents)
+fs.writeFileSync('names-reversed.txt', reversedContents, 'utf-8')
+
+console.log('-- Reversed Names --')
+for (var name of fileArray) {
+  var fileName = 'reversedNames/' + name + '.txt'
+  var nameArray = name.split("")
+  var reversedName = nameArray.reverse().join("")
+  console.log('Reversed Name: ' + reversedName)
+  fs.writeFileSync(fileName, reversedName, 'utf-8')
+}
